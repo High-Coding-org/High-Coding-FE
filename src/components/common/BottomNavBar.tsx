ad01304464932ea406ea8f63145aeb5743f50869
김가연 <121287529+gayeon4679@users.noreply.github.com>
2024-11-24 22:02:29 +0900

    [🎨design]: icon hover 시 동작과 cursor pointer 추가

--- src/components/common/BottomNavBar.tsx
+++ src/components/common/BottomNavBar.tsx
@@ -10,10 +10,11 @@ export default function BottomNavBar() {
   return (
     <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 flex justify-between w-[18.75rem] h-[3.125rem] px-[3rem] items-center rounded-t-[0.5rem] bg-[#007AFD]">
       {icons.map((Icon, index) => (
-        <Icon
+        <a
           key={index}
-          className="text-white w-[1.25rem] h-[1.25rem]"
-        />
+          className="flex items-center justify-center cursor-pointer text-white w-[2.5rem] h-[2.5rem] hover:scale-110 hover:rotate-12 transition-all duration-300">
+          <Icon className="w-[1.25rem] h-[1.25rem]" />
+        </a>
       ))}
     </nav>
   );
