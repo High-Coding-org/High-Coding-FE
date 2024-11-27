b9b87941b8ae0f41f39eb4f14cc2abce304d36f6
김가연 <121287529+gayeon4679@users.noreply.github.com>
2024-11-27 14:35:25 +0900

    [🔥remove]: topNavBar(header의 위치 고정해놓은) 제거

--- src/components/TopNavBar/TopNavBar.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/common/Header';
-
-export default function TopNavBar() {
-  return (
-    <div className="mx-[40px] my-[40px]">
-      <Header />
-    </div>
-  );
-}
