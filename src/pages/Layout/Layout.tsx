8c1a1011731a91105850f6a2f3c0c60bcbe21767
김가연 <121287529+gayeon4679@users.noreply.github.com>
2024-12-23 17:20:24 +0900

    [♻️refactor]:리뷰반영하여 수정
    border-radius추가, ChevronsDown에 cursor-pointer과 클릭 시 애니메이션 추가, Layout 닫히는 애니메이션 추가, Layout 올라오는 애니메이션 추가

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,16 +1,14 @@
 import { ChevronsDown } from 'lucide-react';
-import SideBar from '@/components/common/SideBar';
 import Header from '@/components/common/Header';
-import BottomNav from '@/components/common/BottomNavBar';
+import BottomNavBar from '@/components/common/BottomNavBar';
 import BreadcrumbAndTitle from '@/components/common/Breadcrumb/Breadcrumb';
-const menuItems = [
-  { name: '회원정보 수정', url: '/mypage/profile' },
-  { name: '구매내역', url: '/mypage/purchasehistory' },
-  { name: '탈퇴', url: '/withdrawal' },
-];
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
+
 /**
  * Layout 컴포넌트
  *
@@ -19,23 +17,40 @@ interface LayoutProps {
  * @param {React.ReactNode} children - 페이지별로 다른 내용이 표시되는 영역입니다.
  * @returns {JSX.Element} 기본 레이아웃 구조를 렌더링하는 컴포넌트입니다.
  */
-//! sidebar 파일에서 width값을 설정하는 것으로 변경
-//! 메뉴아이템 url 변경
 export default function Layout({ children }: LayoutProps) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleCollapse = () => setIsCollapsed(!isCollapsed);
+
+  const handleNavClick = (index: number) => {
+    setIsCollapsed(false);
+    const routes = ['/product', '/mykit', '/video', '/event'];
+    navigate(routes[index]);
+  };
+
   return (
-    <div className="min-h-[calc(100vh-1rem)] mt-4 mx-4 pt-4 px-20 pb-[4rem] border border-gray-300">
-      <ChevronsDown className="absolute left-1/2 transform -translate-x-1/2 top-[2rem]" />
-      <Header />
-      <div className="mb-8 mt-8">
-        <BreadcrumbAndTitle />
-      </div>
-      <div className="flex justify-between">
-        <div>{children}</div>
-        <div className="w-[12rem]">
-          <SideBar menuItems={menuItems} />
+    <div className="relative h-screen">
+      <div
+        className={`fixed top-0 left-0 w-[calc(100%_-_2rem)] h-[calc(100vh-1rem)] mt-4 mx-4 pt-4 pb-[4rem] border border-gray-300 rounded-xl overflow-scroll transition-transform duration-500 ease-in-out ${
+          isCollapsed ? 'translate-y-[100%]' : 'translate-y-0'
+        }`}>
+        <div
+          className="flex justify-center cursor-pointer"
+          onClick={toggleCollapse}>
+          <ChevronsDown className="text-gray-300 hover:text-black transition duration-300" />
+        </div>
+
+        <Header />
+
+        <div className="px-[5rem]">
+          <div className="my-8">
+            <BreadcrumbAndTitle />
+          </div>
+          {children}
         </div>
       </div>
-      <BottomNav />
+      <BottomNavBar onIconClick={handleNavClick} />
     </div>
   );
 }
