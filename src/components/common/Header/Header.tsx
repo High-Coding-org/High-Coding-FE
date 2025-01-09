285a638b59a3cc96563ddf54eb5f9762938f9495
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-01-09 21:10:42 +0900

    [🎨design]:메뉴와 아이콘 위치 조정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,7 +3,7 @@ import logo from '@/assets/logo.svg';
 
 /**
  * Header 컴포넌트.
- * 상단 네비게이션 바를 렌더링하며, 로고와 텍스트 메뉴(키트 구매, 내 키트, 키트 영상, 프로모션, 로그인), 아이콘(다크모드, 마이페이지)을 포함합니다.
+ * 상단 네비게이션 바를 렌더링하며, 로고와 텍스트 메뉴(키트 구매, 내 키트, 키트 영상, 프로모션, 로그인), 아이콘(다크모드, 고객 관리, 마이페이지)을 포함합니다.
  */
 export default function Header(): JSX.Element {
   const menuItems = ['키트 구매', '내 키트', '키트 영상', '프로모션', '로그인'];
@@ -11,23 +11,25 @@ export default function Header(): JSX.Element {
 
   return (
     <header className="flex justify-between items-center px-[10rem] h-[3.75rem]">
-      <div>
+      <div className="flex">
         <img
           src={logo}
           alt="Logo"
-          className="w-[8.125rem] h-[3.75rem] cursor-pointer"
+          className="w-[8.125rem] h-[3.75rem] mr-16 cursor-pointer"
         />
+
+        <nav className="flex items-center h-[3.75rem]">
+          {menuItems.map((item, index) => (
+            <span
+              key={index}
+              className="text-sm font-bold cursor-pointer  hover:bg-gray-100 px-4 py-2 rounded">
+              {item}
+            </span>
+          ))}
+        </nav>
       </div>
-      <nav className="flex items-center h-[3.75rem]">
-        {menuItems.map((item, index) => (
-          <span
-            key={index}
-            className="text-sm font-bold cursor-pointer  hover:bg-gray-100 px-4 py-2 rounded">
-            {item}
-          </span>
-        ))}
-      </nav>
-      <div className="flex items-center h-[3.75rem] space-x-4">
+
+      <div className="flex items-center space-x-6">
         {icons.map((Icon, index) => (
           <Icon
             key={index}
