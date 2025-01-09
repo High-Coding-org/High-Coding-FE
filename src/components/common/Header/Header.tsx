1373ac2639ae364ea6c82147ac284a58f8bc4a58
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-01-09 20:43:25 +0900

    [🎨design]: bottomnavbar의 메뉴 topnavbar에 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,29 +1,40 @@
-import { MoonStar, CircleUserRound, Headset, Megaphone } from 'lucide-react';
+import { MoonStar, CircleUserRound, CircleHelp } from 'lucide-react';
 import logo from '@/assets/logo.svg';
 
 /**
  * Header 컴포넌트.
- * 상단 네비게이션 바를 렌더링하며, 로고와 여러 아이콘들(다크모드, 아바타, CS 센터, 공지사항)을 포함합니다.
+ * 상단 네비게이션 바를 렌더링하며, 로고와 텍스트 메뉴(키트 구매, 내 키트, 키트 영상, 프로모션, 로그인), 아이콘(다크모드, 마이페이지)을 포함합니다.
  */
 export default function Header(): JSX.Element {
-  const icons = [MoonStar, CircleUserRound, Headset, Megaphone];
+  const menuItems = ['키트 구매', '내 키트', '키트 영상', '프로모션', '로그인'];
+  const icons = [MoonStar, CircleHelp, CircleUserRound];
+
   return (
-    <header className="flex justify-between items-center px-[5rem] h-[3.75rem]">
+    <header className="flex justify-between items-center px-[10rem] h-[3.75rem]">
       <div>
         <img
           src={logo}
           alt="Logo"
-          className="w-[8.125rem] h-[3.75rem]"
+          className="w-[8.125rem] h-[3.75rem] cursor-pointer"
         />
       </div>
-      <nav className="flex items-center space-x-[2.5rem] h-[3.75rem]">
+      <nav className="flex items-center h-[3.75rem]">
+        {menuItems.map((item, index) => (
+          <span
+            key={index}
+            className="text-sm font-bold cursor-pointer  hover:bg-gray-100 px-4 py-2 rounded">
+            {item}
+          </span>
+        ))}
+      </nav>
+      <div className="flex items-center h-[3.75rem] space-x-4">
         {icons.map((Icon, index) => (
           <Icon
             key={index}
             className="w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300"
           />
         ))}
-      </nav>
+      </div>
     </header>
   );
 }
