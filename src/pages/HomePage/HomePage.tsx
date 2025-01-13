0e28019323db338c92b7e4e452e257ec35c577c5
타래 <85001878+meowTarae@users.noreply.github.com>
2025-01-13 10:41:14 +0900

    [✨feat]: 홈페이지-첫번째 섹션 구현

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <main className="absolute w-screen">
+      <section className="w-full ">
+        {/* // Todo: div를 이미지로 변경해야 함 */}
+        <div className="relative h-screen ">
+          <h1 className="absolute text-6xl font-bold leading-normal text-center whitespace-pre-line -translate-x-1/2 left-1/2 top-[30%]">
+            금융의 모든 것
+            <br />
+            토스에서 쉽고 간편하게
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
+}
