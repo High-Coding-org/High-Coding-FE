6a8ea0cc68e3ad4d2dc158a0d05157983f4e8132
타래 <85001878+meowTarae@users.noreply.github.com>
2025-01-13 13:35:55 +0900

    [♻️refactor]: 반응형을 위한 홈 타이틀 리팩토링

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,14 @@ import './bounceAnimation.css';
 import { ChevronDown } from 'lucide-react';
 import { useRef } from 'react';
 
+import ResponsiveText from './components/ResponsiveText';
+import {
+  HomeSubTitle,
+  HomeSubTitleClass,
+  HomeTitle,
+  HomeTitleClass,
+} from './constants';
+
 export default function HomePage() {
   const subTitle = useRef<HTMLDivElement>(null);
 
@@ -14,27 +22,37 @@ export default function HomePage() {
     <main className="absolute w-screen">
       <section className="w-full ">
         {/* // Todo: div를 이미지로 변경해야 함 */}
-        <div className="relative h-screen ">
-          <h1 className="absolute text-6xl font-bold leading-normal text-center whitespace-pre-line -translate-x-1/2 left-1/2 top-[30%]">
-            금융의 모든 것
-            <br />
-            토스에서 쉽고 간편하게
-          </h1>
+        <div className="relative h-screen">
+          <div className="absolute -translate-x-1/2 left-1/2 top-[30%]">
+            {HomeTitle.map((text, index) => (
+              <ResponsiveText
+                key={index}
+                text={text}
+                breakPoint="|"
+                className={HomeTitleClass}
+              />
+            ))}
+          </div>
+
           <ChevronDown
             className="absolute w-16 h-16 p-4 -translate-x-1/2 opacity-50 hover:cursor-pointer left-1/2 bottom-10 bounce-animation"
             onClick={handleScrollToSubTitle}
           />
         </div>
+
         <div
           ref={subTitle}
           className="bg-[#F9FAFB] h-[600px] relative">
-          <h3 className="absolute text-3xl font-semibold leading-normal text-center whitespace-pre-line -translate-x-1/2 left-1/2 top-1/3">
-            내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
-            <br />
-            이제껏 경험 못 했던 쉽고 편리한 금융 서비스
-            <br />
-            토스와 함께라면 당신의 일상이 새로워질 거예요.
-          </h3>
+          <div className="absolute grid w-full grid-cols-1 gap-2 -translate-x-1/2 left-1/2 top-1/3">
+            {HomeSubTitle.map((text, index) => (
+              <ResponsiveText
+                key={index}
+                text={text}
+                breakPoint="|"
+                className={HomeSubTitleClass}
+              />
+            ))}
+          </div>
         </div>
       </section>
       <section className="h-[1200px] bg-green-500">세번째 박스</section>
