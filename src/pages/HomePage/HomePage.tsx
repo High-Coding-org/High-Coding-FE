71683b71b4a571f0e752336fd66c1a8b2b60f72e
타래 <85001878+meowTarae@users.noreply.github.com>
2025-01-13 10:45:29 +0900

    [✨feat]: 메인-두번째 섹션 구현

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -25,6 +25,17 @@ export default function HomePage() {
             onClick={handleScrollToSubTitle}
           />
         </div>
+        <div
+          ref={subTitle}
+          className="bg-[#F9FAFB] h-[600px] relative">
+          <h3 className="absolute text-3xl font-semibold leading-normal text-center whitespace-pre-line -translate-x-1/2 left-1/2 top-1/3">
+            내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
+            <br />
+            이제껏 경험 못 했던 쉽고 편리한 금융 서비스
+            <br />
+            토스와 함께라면 당신의 일상이 새로워질 거예요.
+          </h3>
+        </div>
       </section>
       <section className="h-[1200px] bg-green-500">세번째 박스</section>
       {/* <section></section> */}
