e3dc5c47706a03281033feec82367c6d91843ae4
타래 <85001878+meowTarae@users.noreply.github.com>
2025-01-21 12:25:31 +0900

    [✨feat]: 헤더 컴포넌트 스크롤 내릴 시 테두리 생성

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { CircleHelp, CircleUserRound, Menu, MoonStar, X } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { ADDITIONAL_MENU_ITEMS, MENU_ITEMS } from '@/constants/header';
 
@@ -13,66 +13,83 @@ import Logo from '../Logo/Logo';
 export default function Header() {
   const icons = [MoonStar, CircleHelp, CircleUserRound];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (isScroll && window.scrollY != 0) return;
+
+      setIsScroll(window.scrollY != 0);
+    };
+
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isScroll]);
+
   return (
-    <header className="flex justify-between items-center w-pageWidth h-[3.75rem] whitespace-nowrap px-5 sticky top-0 z-header">
-      <div className="flex">
-        <Logo style="w-[8.125rem] h-[3.75rem]" />
+    <div
+      className={`flex sticky justify-center w-full top-0 z-header bg-white px-5 ${isScroll ? 'border-b border-gray-300' : ''} `}>
+      <header
+        className={`flex justify-between items-center w-pageWidth h-[3.75rem] whitespace-nowrap `}>
+        <div className="flex">
+          <Logo style="w-[8.125rem] h-[3.75rem]" />
 
-        <nav className="flex items-center h-[3.75rem] ">
-          {MENU_ITEMS.map((item, index) => (
-            <span
-              key={index}
-              className="hidden px-4 py-2 text-sm font-bold rounded cursor-pointer md:block hover:bg-gray-100">
-              {item}
-            </span>
-          ))}
-        </nav>
-      </div>
+          <nav className="flex items-center h-[3.75rem] ">
+            {MENU_ITEMS.map((item, index) => (
+              <span
+                key={index}
+                className="hidden px-4 py-2 text-sm font-bold rounded cursor-pointer md:block hover:bg-gray-100">
+                {item}
+              </span>
+            ))}
+          </nav>
+        </div>
 
-      <div className="flex items-center space-x-6">
-        {icons.map((Icon, index) => (
-          <Icon
-            key={index}
-            className="hidden md:block w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300"
-          />
-        ))}
-        {isMenuOpen ? (
-          <X
-            onClick={toggleMenu}
-            className="md:hidden w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300"
-          />
-        ) : (
-          <Menu
-            onClick={toggleMenu}
-            className="md:hidden w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300 "
-          />
-        )}
-      </div>
+        <div className="flex items-center space-x-6">
+          {icons.map((Icon, index) => (
+            <Icon
+              key={index}
+              className="hidden md:block w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300"
+            />
+          ))}
+          {isMenuOpen ? (
+            <X
+              onClick={toggleMenu}
+              className="md:hidden w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300"
+            />
+          ) : (
+            <Menu
+              onClick={toggleMenu}
+              className="md:hidden w-[1.5rem] h-[1.5rem] cursor-pointer hover:text-gray-500 transition duration-300 "
+            />
+          )}
+        </div>
 
-      <div
-        className={`absolute top-[3.75rem] left-0 w-full h-full bg-white shadow-md transition-max-height duration-500 ease overflow-hidden  ${isMenuOpen ? 'max-h-screen' : 'max-h-0'}`}>
-        <nav className="flex flex-col items-start p-4 mx-4 space-y-2 transform">
-          {MENU_ITEMS.map((item, index) => (
-            <span
-              key={index}
-              className="w-full px-4 py-2 text-sm font-bold text-left rounded cursor-pointer hover:bg-gray-100">
-              {item}
-            </span>
-          ))}
-          {ADDITIONAL_MENU_ITEMS.map((item, index) => (
-            <span
-              key={index}
-              className="w-full px-4 py-2 text-sm font-bold text-left rounded cursor-pointer hover:bg-gray-100">
-              {item}
-            </span>
-          ))}
-        </nav>
-      </div>
-    </header>
+        <div
+          className={`absolute top-[3.75rem] left-0 w-full h-full bg-white shadow-md transition-max-height duration-500 ease overflow-hidden  ${isMenuOpen ? 'max-h-screen' : 'max-h-0'}`}>
+          <nav className="flex flex-col items-start p-4 mx-4 space-y-2 transform">
+            {MENU_ITEMS.map((item, index) => (
+              <span
+                key={index}
+                className="w-full px-4 py-2 text-sm font-bold text-left rounded cursor-pointer hover:bg-gray-100">
+                {item}
+              </span>
+            ))}
+            {ADDITIONAL_MENU_ITEMS.map((item, index) => (
+              <span
+                key={index}
+                className="w-full px-4 py-2 text-sm font-bold text-left rounded cursor-pointer hover:bg-gray-100">
+                {item}
+              </span>
+            ))}
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 }
