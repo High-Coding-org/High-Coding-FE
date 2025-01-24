c0974c619bc3c70a87bdcac513ec803117218079
타래 <85001878+meowTarae@users.noreply.github.com>
2025-01-24 10:41:45 +0900

    [🐛fix]: 전역 통신 오류 제어 코드 제거
    kitDetail에서 get 요청이 실패 => 페이지 전체가 보여지지않는 크리티컬한 오류
    but, 이는 내 프로필에서 주소 등록같은 간단한 작업에서도 홈페이지로 이동시키는 치명적인 오류가 내포되어 있음. 이를 수정하고자 함

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,15 @@
 import './index.css';
 
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { AxiosError } from 'axios';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import ToastProvider from './components/common/ToastProvider/ToastProvider';
-import { useGlobalErrorStore } from './store/globalErrorStore';
 
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error: AxiosError) => {
-      useGlobalErrorStore.getState().globalErrorOccur();
-
-      switch (error.message) {
-        case 'Network Error':
-          useGlobalErrorStore
-            .getState()
-            .setGlobalErrorMsg('네트워크 에러가 발생했습니다');
-          break;
-        default:
-          useGlobalErrorStore
-            .getState()
-            .setGlobalErrorMsg('정보를 불러오는데 실패했습니다');
-          break;
-      }
-    },
-  }),
-});
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
