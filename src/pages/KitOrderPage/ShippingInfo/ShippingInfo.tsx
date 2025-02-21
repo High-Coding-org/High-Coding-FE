8e644a4c2db1f7a5521fa9c906df16d2fcec770a
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-21 12:22:38 +0900

    [💬comment]: 페이지에 대한 내용을 주석으로 추가

--- src/pages/KitOrderPage/ShippingInfo/ShippingInfo.tsx
+++ src/pages/KitOrderPage/ShippingInfo/ShippingInfo.tsx
@@ -1,5 +1,6 @@
 import { Label } from '@/components/ui/label';
 
+import DeliveryAddress from './DeliveryAddress';
 import DeliveryNote from './DeliveryNote';
 import ShippingUser from './ShippingUser';
 
@@ -10,10 +11,10 @@ interface ShippingInfoProps {
   setDeliveryNote: (deliveryNote: string) => void;
 }
 
-/*
-  1. 부모에게 set배송메모 를 인자로 받는다.
-  2. 그리고 이 컴포넌트에서 배송메모를 변경할 때마다 부모에게 전달한다.
-*/
+/**
+ * 유저에게 배송할 정보를 보여주는 컴포넌트 입니다.
+ * 유저명, 전화번호, 배송지 주소, 배송메모를 표시합니다.
+ */
 
 export default function ShippingInfo({
   name,
@@ -26,13 +27,14 @@ export default function ShippingInfo({
       <Label className="pl-4 font-bold">배송 정보</Label>
 
       <div className="flex flex-col gap-2 px-6 py-6 bg-white border border-gray-200 rounded-lg shadow-md">
+        {/* 배송자 정보 */}
         <ShippingUser
           name={name}
           phoneNumber={phoneNumber}
         />
 
-        {/* 배송지 주소, daumPostcode 컴포넌트 사용 */}
-        {/* <DaumPostCode /> */}
+        {/* 배송지 주소 */}
+        <DeliveryAddress />
 
         {/* 배송 메모 */}
         <DeliveryNote
