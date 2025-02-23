cf21902d141d567eebe5254a3a94362836a4b1ec
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-23 22:42:52 +0900

    [♻️refactor]: PaymentMethod 컨테이너 리팩토링

--- src/pages/KitOrderPage/PaymentMethod/PaymentMethod.tsx
+++ src/pages/KitOrderPage/PaymentMethod/PaymentMethod.tsx
@@ -2,6 +2,8 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { PAYMENT_METHODS } from '@/constants/paymentMethods';
 
+import SectionContainer from '../components/SectionContainer';
+
 interface PaymentMethodProps {
   setPaymentMethod: (method: string) => void;
 }
@@ -10,31 +12,29 @@ export default function PaymentMethod({
   setPaymentMethod,
 }: PaymentMethodProps) {
   return (
-    <div className="flex flex-col gap-4 mb-6">
-      <Label className="pl-4 font-bold">결제 수단</Label>
-
-      <div className="px-6 py-6 bg-white border border-gray-200 rounded-lg shadow-md">
-        <RadioGroup
-          className="gap-0"
-          defaultValue={PAYMENT_METHODS[0].id}
-          onValueChange={value => setPaymentMethod(value)}>
-          {PAYMENT_METHODS.map((method, index) => (
-            <div
-              key={index + method.id}
-              className="flex items-center h-12 gap-2 border-b-2 last:border-none">
-              <RadioGroupItem
-                value={method.label}
-                id={method.id}
-              />
-              <Label
-                htmlFor={method.id}
-                className="font-bold hover:cursor-pointer">
-                {method.label}
-              </Label>
-            </div>
-          ))}
-        </RadioGroup>
-      </div>
-    </div>
+    <SectionContainer
+      label="결제 수단"
+      className="p-6">
+      <RadioGroup
+        className="gap-0"
+        defaultValue={PAYMENT_METHODS[0].id}
+        onValueChange={value => setPaymentMethod(value)}>
+        {PAYMENT_METHODS.map((method, index) => (
+          <div
+            key={index + method.id}
+            className="flex items-center h-12 gap-2 border-b-2 last:border-none">
+            <RadioGroupItem
+              value={method.label}
+              id={method.id}
+            />
+            <Label
+              htmlFor={method.id}
+              className="font-bold hover:cursor-pointer">
+              {method.label}
+            </Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </SectionContainer>
   );
 }
