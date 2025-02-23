2eb15bd1279983c111f630b1512d9af9a79e8b6b
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-23 14:06:00 +0900

    [♻️refactor]: 코드 퀄리티 개선
    결제수단 컴포넌트
    hr를 border-b-2로 수정

--- src/pages/KitOrderPage/KitOrderPage.tsx
+++ src/pages/KitOrderPage/KitOrderPage.tsx
@@ -37,14 +37,14 @@ export default function KitOrderPage() {
   const [regionalAddress, setRegionalAddress] = useState<string>('');
   const [detailedAddress, setDetailedAddress] = useState<string>('');
   const [discount, setDiscount] = useState<number>(0);
+  const [paymentMethod, setPaymentMethod] = useState<string>('');
   // console.log('kitOrderPage에서 DN : ', deliveryNote);
   // console.log('kitOrderPage에서 지역 주소 : ', regionalAddress);
   // console.log('kitOrderPage에서 상세 주소 : ', detailedAddress);
+  // console.log('kitOrderPage에서 결제 수단 : ', paymentMethod);
 
   //결제 금액 (키트 가격 x 수량)
   const totalPrice = DUMMY_ORDER_ITEMS.price * DUMMY_ORDER_ITEMS.quantity;
-  // ! 결제 금액 계산 => 없어질 놈
-  const totalPayment = totalPrice + 3000;
 
   // todo: 비정상적 접근시 home으로 redirect 하는 코드 구현
   // useEffect(() => {
@@ -57,7 +57,6 @@ export default function KitOrderPage() {
 
   return (
     <>
-      {/* BreadCrumb */}
       {/* <BreadcrumbAndTitle /> */}
 
       <main className="flex justify-between h-full w-pageWidth">
@@ -89,12 +88,13 @@ export default function KitOrderPage() {
           />
 
           {/* 결제수단 */}
-          <PaymentMethod />
+          <PaymentMethod setPaymentMethod={setPaymentMethod} />
         </section>
 
         {/* 결제 정보 */}
         <nav className="sticky w-[25%] flex flex-col gap-4 top-24 h-fit ">
           <Label className="pl-4 font-bold">결제 정보</Label>
+
           <div className="flex flex-col gap-4 px-6 py-6 bg-white border border-gray-200 rounded-lg shadow-md">
             <div className="flex justify-between text-sm">
               <span>상품금액</span>
@@ -102,16 +102,18 @@ export default function KitOrderPage() {
             </div>
             <div className="flex justify-between text-sm">
               <span>배송비</span>
-              <span>{formatMoneyKR(data.shippingFee)}</span>
+              {/* // todo: 배송비 실제 데이터로 교체 */}
+              <span>{formatMoneyKR(3000)}</span>
             </div>
             <div className="flex justify-between text-sm">
               <span>할인</span>
-              <span>-{formatMoneyKR(data.discount)}</span>
+              <span>- {formatMoneyKR(discount)}</span>
             </div>
             <hr />
             <div className="flex justify-between text-sm font-bold">
               <span>결제금액</span>
-              <span>{formatMoneyKR(totalPayment)}</span>
+              {/* // todo: 배송비 실제 데이터로 교체 */}
+              <span>{formatMoneyKR(totalPrice + 3000 - discount)}</span>
             </div>
           </div>
           <Button className="bg-[#007AFD] hover:bg-blue-800">결제하기</Button>
--- src/pages/KitOrderPage/PaymentMethod/PaymentMethod.tsx
+++ src/pages/KitOrderPage/PaymentMethod/PaymentMethod.tsx
@@ -2,30 +2,41 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { PAYMENT_METHODS } from '@/constants/paymentMethods';
 
-export default function PaymentMethod() {
+interface PaymentMethodProps {
+  setPaymentMethod: (method: string) => void;
+}
+
+export default function PaymentMethod({
+  setPaymentMethod,
+}: PaymentMethodProps) {
   return (
-    <section className="flex flex-col gap-4 mb-6">
+    <div className="flex flex-col gap-4 mb-6">
       <Label className="pl-4 font-bold">결제수단</Label>
+
       <div className="px-6 py-6 bg-white border border-gray-200 rounded-lg shadow-md">
-        <RadioGroup defaultValue="option-one">
+        <RadioGroup
+          className="gap-0"
+          defaultValue={PAYMENT_METHODS[0].id}
+          onValueChange={value => setPaymentMethod(value)}>
           {PAYMENT_METHODS.map((method, index) => (
-            <div key={method.id}>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value={method.id}
-                  id={method.id}
-                />
-                <Label
-                  htmlFor={method.id}
-                  className="font-bold">
-                  {method.label}
-                </Label>
-              </div>
-              {index < PAYMENT_METHODS.length - 1 && <hr className="my-3" />}
+            <div
+              key={index + method.id}
+              className={`flex items-center h-12 gap-2 ${
+                index === PAYMENT_METHODS.length - 1 ? '' : 'border-b-2'
+              }`}>
+              <RadioGroupItem
+                value={method.label}
+                id={method.id}
+              />
+              <Label
+                htmlFor={method.id}
+                className="font-bold hover:cursor-pointer">
+                {method.label}
+              </Label>
             </div>
           ))}
         </RadioGroup>
       </div>
-    </section>
+    </div>
   );
 }
