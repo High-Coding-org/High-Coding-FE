c20fd193781591b2449e79cf5e9b222a26b98fc4
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-23 23:32:20 +0900

    [✨feat]: 배송메모-직접 입력하기 선택 시 Input focus 기능 구현

--- src/pages/KitOrderPage/ShippingInfo/DeliveryNote.tsx
+++ src/pages/KitOrderPage/ShippingInfo/DeliveryNote.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Input } from '@/components/ui/input';
 import {
@@ -20,6 +20,8 @@ export default function DeliveryNote({
   setDeliveryNote,
 }: DeliveryNoteProps) {
   const [inputMode, setInputMode] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleDeliveryNoteChange = (value: string) => {
     if (value === '직접 입력하기') {
       setDeliveryNote('');
@@ -29,10 +31,19 @@ export default function DeliveryNote({
       setInputMode(false);
     }
   };
+
+  useEffect(() => {
+    if (!inputMode) return;
+
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 100);
+  }, [inputMode]);
+
   return (
     <>
       <Select onValueChange={handleDeliveryNoteChange}>
-        <SelectTrigger>
+        <SelectTrigger className="focus-visible:ring-0 focus:ring-0">
           <SelectValue placeholder={DELIVERY_PLACEHOLDER.SELECT} />
         </SelectTrigger>
         <SelectContent>
@@ -47,6 +58,7 @@ export default function DeliveryNote({
       </Select>
       {inputMode && (
         <Input
+          ref={inputRef}
           type="text"
           placeholder={DELIVERY_PLACEHOLDER.INPUT}
           onChange={e => setDeliveryNote(e.target.value)}
