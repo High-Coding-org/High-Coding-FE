eacd67fcdb1023bb9878e879c9cdb31dc0d86753
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-23 14:45:04 +0900

    [♻️refactor]: 결제정보 - 중복되는 코드 부분 리팩토링

--- src/pages/KitOrderPage/PriceInfo/PriceInfo.tsx
+++ src/pages/KitOrderPage/PriceInfo/PriceInfo.tsx
@@ -7,30 +7,37 @@ interface PriceInfoProps {
 }
 
 export default function PriceInfo({ totalPrice, discount }: PriceInfoProps) {
+  // todo: 회의를 통해 배송비를 결정하고, 배송비를 constants 폴더로 이동할 것.
+  const DELIVERY_SHIPPING_FEE = 3000;
+
+  const finalAmount = totalPrice + DELIVERY_SHIPPING_FEE - discount;
+  const priceItems = [
+    { label: '상품금액', amount: totalPrice },
+    { label: '배송비', amount: DELIVERY_SHIPPING_FEE },
+    { label: '할인', amount: -discount, isNegative: true },
+    { label: '결제금액', amount: finalAmount, isNegative: true },
+  ];
+
   return (
     <>
       <Label className="pl-4 font-bold">결제 정보</Label>
 
       <div className="flex flex-col gap-4 px-6 py-6 bg-white border border-gray-200 rounded-lg shadow-md">
-        <div className="flex justify-between text-sm">
-          <span>상품금액</span>
-          <span>{formatMoneyKR(totalPrice)}</span>
-        </div>
-        <div className="flex justify-between text-sm">
-          <span>배송비</span>
-          {/* // todo: 배송비 실제 데이터로 교체 */}
-          <span>{formatMoneyKR(3000)}</span>
-        </div>
-        <div className="flex justify-between text-sm">
-          <span>할인</span>
-          <span>- {formatMoneyKR(discount)}</span>
-        </div>
-        <hr />
-        <div className="flex justify-between text-sm font-bold">
-          <span>결제금액</span>
-          {/* // todo: 배송비 실제 데이터로 교체 */}
-          <span>{formatMoneyKR(totalPrice + 3000 - discount)}</span>
-        </div>
+        {priceItems.map(({ label, amount, isNegative }, index) => (
+          <>
+            <div
+              key={label}
+              className="flex justify-between text-sm last:font-bold">
+              <span>{label}</span>
+              <span>
+                {isNegative && '- '}
+                {formatMoneyKR(Math.abs(amount))}
+              </span>
+            </div>
+
+            {index === priceItems.length - 2 && <hr />}
+          </>
+        ))}
       </div>
     </>
   );
