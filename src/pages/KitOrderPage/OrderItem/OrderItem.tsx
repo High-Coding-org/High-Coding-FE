90cca05922f8ded217b7119c2b008d4c760ba595
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-23 22:29:58 +0900

    [♻️refactor]: OrderItem 컨테이너 리팩토링

--- src/pages/KitOrderPage/OrderItem/OrderItem.tsx
+++ src/pages/KitOrderPage/OrderItem/OrderItem.tsx
@@ -1,5 +1,6 @@
-import { Label } from '@/components/ui/label';
 import { formatMoneyKR } from '@/utils/formatMoneyKR';
+
+import SectionContainer from '../components/SectionContainer';
 
 interface OrderItemProps {
   productName: string;
@@ -13,16 +14,12 @@ export default function OrderItem({
   quantity,
 }: OrderItemProps) {
   return (
-    <div className="flex flex-col gap-4 mb-6">
-      <Label className="pl-4 font-bold">주문상품</Label>
-
-      <div className="px-6 py-6 mb-4 bg-white border border-gray-200 rounded-lg shadow-md">
-        <h3 className="text-base font-bold">{productName}</h3>
-        <p className="my-1 text-sm">수량: {quantity}개</p>
-        <p className="text-xs text-gray-500">
-          키트 가격: {formatMoneyKR(price)}
-        </p>
-      </div>
-    </div>
+    <SectionContainer
+      label="주문 상품"
+      className="p-6">
+      <h3 className="text-base font-bold">{productName}</h3>
+      <p className="my-1 text-sm">수량: {quantity}개</p>
+      <p className="text-xs text-gray-500">키트 가격: {formatMoneyKR(price)}</p>
+    </SectionContainer>
   );
 }
