cda7f92e9efa635753aa6adb64c8023a9e5cbb2e
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-02-24 08:12:16 +0900

    [♻️refactor]: 에러,성공 메세지 출력 부분 passwordChange.tsx로 이동

--- src/pages/PasswordChange/usePasswordChange.ts
+++ src/pages/PasswordChange/usePasswordChange.ts
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import { useMutation } from '@tanstack/react-query';
-import { toast } from 'react-toastify';
 import { changePassword } from '@/services/password/changePassword';
 export const usePasswordChange = () => {
   const mutation = useMutation<
@@ -10,19 +8,12 @@ export const usePasswordChange = () => {
   >({
     mutationFn: async ({ currentPassword, newPassword }) => {
       const token = localStorage.getItem('authToken');
-      console.log(token);
       const response = await changePassword(token, {
         currentPassword,
         newPassword,
       });
-      console.log(response);
-      return response.data;
-    },
-    onSuccess: data => {
-      toast.success(data);
-    },
-    onError: error => {
-      toast.error('비밀번호 변경 실패:' + error.message);
+
+      return response.data.message;
     },
   });
 
