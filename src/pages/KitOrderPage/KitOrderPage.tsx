a0af690cc62bbbc6fec2a8d1f8229a51e3ff87ac
타래 <85001878+meowTarae@users.noreply.github.com>
2025-02-24 09:29:16 +0900

    [💄style]: 버튼 css 및 html 태그 변경

--- src/pages/KitOrderPage/KitOrderPage.tsx
+++ src/pages/KitOrderPage/KitOrderPage.tsx
@@ -31,7 +31,7 @@ export default function KitOrderPage() {
 
       <main className="flex justify-between h-full w-pageWidth text-[#222]">
         {/* 주문 정보 */}
-        <article className="w-[70%]">
+        <div className="w-[70%]">
           {/* 배송지 */}
           <ShippingInfo
             name={DUMMY_PURCHASE_DATA.name}
@@ -42,7 +42,6 @@ export default function KitOrderPage() {
             setDetailedAddress={setDetailedAddress}
           />
 
-          {/* // todo: 더미데이터를 실제 데이터로 교체해야 함. */}
           {/* 주문상품 */}
           <OrderItem
             productName={productName}
@@ -59,7 +58,7 @@ export default function KitOrderPage() {
 
           {/* 결제 수단 */}
           <PaymentMethod setPaymentMethod={setPaymentMethod} />
-        </article>
+        </div>
 
         {/* 결제 정보 */}
         <nav className="sticky w-[25%] flex flex-col top-14 h-fit ">
@@ -67,7 +66,7 @@ export default function KitOrderPage() {
             totalPrice={totalPrice}
             discount={discount}
           />
-          <Button className="bg-[#007AFD] hover:bg-blue-800">결제하기</Button>
+          <Button className="bg-primary active:bg-blue-800">결제하기</Button>
         </nav>
       </main>
     </>
