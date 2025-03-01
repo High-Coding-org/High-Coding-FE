cf1e378080a75601717c063ae278ef1461de57bc
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-03-02 04:51:10 +0900

    [📝docs]: 경로별 제목 수정

--- src/routes/Breadcrumb/pathToTitles.ts
+++ src/routes/Breadcrumb/pathToTitles.ts
@@ -2,13 +2,12 @@ import { PATH } from '@/routes/path';
 
 // 경로별 제목 정의
 export const PATH_TO_TITLES: Record<keyof typeof PATH, string> = {
-  SIGN: '로그인 & 회원가입',
   PROFILE: '회원 정보',
-  PROFILE_MY_ORDER: '주문 내역',
-  PRODUCT: '상품',
-  PRODUCT_PURCHASE: '구매하기',
+  PROFILE_MY_ORDER: '구매 내역',
+  PRODUCT: '키트 구매',
+  PRODUCT_PURCHASE: '주문/결제',
   PLANT: '식물',
-  PLANT_RECOMMEND: '추천 식물',
+  PLANT_RECOMMEND: 'AI 식물 추천',
   PLANT_REGISTER: '식물 등록',
-  PLANT_DETAIL: '식물 상세 정보',
+  PLANT_DETAIL: '식물 성장',
 };
