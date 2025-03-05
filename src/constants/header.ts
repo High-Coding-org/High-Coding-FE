d823d1ba113db786170f8fd2aa08b84730dd8c9a
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-05 21:15:45 +0900

    [✨feat]: routes path에 맞게끔 constants 헤더 파일 수정

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -1,3 +1,15 @@
-export const MENU_ITEMS = ['키트 구매', '내 키트', '키트 영상', '프로모션'];
+export const MENU_ITEMS = [
+  '키트 정보',
+  '나의 식물',
+  '식물 지식백과',
+  'AI 식물 추천',
+];
 
-export const ADDITIONAL_MENU_ITEMS = ['계정', '고객 센터'];
+export const NAME_TO_PATH = {
+  '키트 정보': '/product',
+  '나의 식물': '/plant/myPlant',
+  '식물 지식백과': '/plant/dictionary',
+  'AI 식물 추천': '/plant/AIRecommend',
+};
+
+export const ADDITIONAL_MENU_ITEMS = ['내 정보', '고객 센터'];
