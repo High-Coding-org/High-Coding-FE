69207e4fc3d3a443a1243f18b7ce21c11c388574
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-05 22:10:06 +0900

    [🐛fix]: Logo 깨지는 현상 에러 픽스

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,18 +1,18 @@
 import { useNavigate } from 'react-router';
 
 import { cn } from '@/lib/utils';
-
 interface LogoProps {
   style: string;
 }
 
 export default function Logo({ style }: LogoProps) {
   const navigate = useNavigate();
+  const logoUrl = new URL('@/assets/logo.svg', import.meta.url).href;
 
   return (
     <>
       <img
-        src={'src/assets/logo.svg'}
+        src={logoUrl}
         alt="HiCoding-Logo"
         onClick={() => navigate('/')}
         className={cn(style, 'cursor-pointer')}
