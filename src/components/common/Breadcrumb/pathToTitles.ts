f6853e36ace092717d9e31d5a520ccc5de13c181
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-05 19:20:47 +0900

    [🐛fix]: path url 이름 수정
    purchase -> order

--- src/components/common/Breadcrumb/pathToTitles.ts
+++ src/components/common/Breadcrumb/pathToTitles.ts
@@ -5,7 +5,7 @@ export const PATH_TO_TITLES: Record<keyof typeof PATH, string> = {
   PROFILE: '회원 정보',
   PROFILE_MY_ORDER: '구매 내역',
   PRODUCT: '키트 구매',
-  PRODUCT_PURCHASE: '주문/결제',
+  PRODUCT_ORDER: '주문 / 결제',
   PLANT: '식물',
   PLANT_RECOMMEND: 'AI 식물 추천',
   PLANT_REGISTER: '식물 등록',
