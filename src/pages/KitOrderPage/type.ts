1d4e915d521362b83ba112698481c6ab893f3f6c
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-07 21:46:40 +0900

    [🔥remove]: 더미 타입들 제거

--- src/pages/KitOrderPage/type.ts
+++ src/pages/KitOrderPage/type.ts
@@ -1,29 +1,3 @@
-interface OrderItem {
-  productName: string;
-  price: number;
-  quantity: number;
-}
-
-interface PurchaseData {
-  name: string;
-  phoneNumber: string;
-  shippingAddress: string;
-  discount: number;
-  shippingFee: number;
-}
-
-interface Coupon {
-  name: string;
-  discountPercent: number;
-  discount: number;
-}
-
-export interface OrderProps {
-  purchaseData: PurchaseData;
-  orderItems: OrderItem;
-  coupons: Coupon[];
-  addresses: string[];
-}
 export interface kitOrderValues {
   deliveryNote: string;
   regionalAddress: string;
