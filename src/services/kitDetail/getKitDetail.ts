9b2a4a5c19dcd2ef95d40a8e96d435b6453773e6
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-07 20:40:44 +0900

    [✨feat]: api endpoint error fix

--- src/services/kitDetail/getKitDetail.ts
+++ src/services/kitDetail/getKitDetail.ts
@@ -1,13 +1,12 @@
 import { KitResponse } from '@/types/KitDetail/kitDetail';
 
-import { API_ENDPOINT } from '../apiEndpoint';
+import { API_AUTHORITY, API_ENDPOINT } from '../apiEndpoint';
 import { axiosInstance } from '../axiosInstance';
 
 export const getKitDetail = async (id: number) => {
   try {
     const res: KitResponse = await axiosInstance.get(
-      // `${API_ENDPOINT.PRODUCT.KIT}/${id}`
-      `${API_ENDPOINT.PRODUCT.KIT}`
+      `${API_AUTHORITY.PUBLIC}${API_ENDPOINT.PRODUCT.KIT}${id}`
     );
 
     return res?.data;
