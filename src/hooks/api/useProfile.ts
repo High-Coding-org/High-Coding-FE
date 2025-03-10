8ef47db9543583e867b70dbec8799247e37c7e48
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-03-10 21:31:46 +0900

    [♻️refactor]: 'authToken'을 LOCAL_STORAGE_AUTH_TOKEN 상수로 변경

--- src/hooks/api/useProfile.ts
+++ src/hooks/api/useProfile.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProfile } from '@/services/profile/getProfile';
+import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
 
 import { IProfile } from '../../pages/ProfilePage/type';
 
 export const useProfile = () => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN);
 
   return useQuery<IProfile>({
     queryKey: ['profileData', token],
