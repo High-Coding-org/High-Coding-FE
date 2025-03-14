44f3dc69d8aa0469963567cc45a773ef4799ccea
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-14 14:31:23 +0900

    [🔥remove]: 불필요한 파일 삭제

--- src/routes/CheckUserLogin.tsx
+++
@@ -1,7 +0,0 @@
-import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
-
-export const CheckUserLogin = () => {
-  const accessToken = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN);
-
-  return accessToken;
-};
