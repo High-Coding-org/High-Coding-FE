b8fd3025789efaf282a3d925bcad7e668df4f7e2
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-14 13:30:07 +0900

    [💬comment]: 불필요한 주석 제거

--- src/services/auth/signIn.ts
+++ src/services/auth/signIn.ts
@@ -4,7 +4,6 @@ import { SignInResponse } from '@/types/Login/signIn';
 import { API_AUTHORITY, API_ENDPOINT } from '../apiEndpoint';
 import { axiosInstance } from '../axiosInstance';
 
-// ! 스웨거 속성 값 변경 시 username -> id로 변경해야 할 수 있음.
 export const signIn = async ({ username, password }: SignInFormData) => {
   const reqBody = {
     username,
