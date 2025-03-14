ee5fcb2b340213b361a254dceda96108fbe52dbf
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-03-15 00:06:27 +0900

    [♻️refactor]: IProfile 타입을 IProfileData로 변경

--- src/hooks/api/useProfile.ts
+++ src/hooks/api/useProfile.ts
@@ -2,12 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { getProfile } from '@/services/profile/getProfile';
 import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
 
-import { IProfile } from '../../pages/ProfilePage/type';
+import { IProfileData } from '../../pages/ProfilePage/type';
 
 export const useProfile = () => {
   const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN);
 
-  return useQuery<IProfile>({
+  return useQuery<IProfileData>({
     queryKey: ['profileData', token],
     queryFn: () => getProfile(token),
     retry: false,
