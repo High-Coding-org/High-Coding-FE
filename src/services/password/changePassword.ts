e8a226b7386837cb36cb538fee0874b99c78da90
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-03-15 00:09:21 +0900

    [♻️refactor]: changePassword.ts 리팩토링
    하드코딩된 서버 url 수정, axios.put을 axiosInstance로 수정, try catch문 추가

--- src/services/password/changePassword.ts
+++ src/services/password/changePassword.ts
@@ -3,21 +3,30 @@ interface PasswordData {
   newPassword: string;
 }
 import axios from 'axios';
+import { AxiosResponse } from 'axios';
+import { IPasswordChangeData } from '@/pages/PasswordChangePage/type';
+import { axiosInstance } from '../axiosInstance';
+import { API_AUTHORITY, API_ENDPOINT } from '../apiEndpoint';
+
+type PasswordResponse = AxiosResponse<IPasswordChangeData>;
 
 export const changePassword = async (
   token: string,
   passwordData: PasswordData
 ) => {
-  const response = await axios.put(
-    'https://leoan.p-e.kr/user/changePassword',
-    passwordData,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  try {
+    const response: PasswordResponse = await axiosInstance.put(
+      `${API_AUTHORITY.USER}${API_ENDPOINT.PASSWORD}`,
+      passwordData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-  return response;
+    return response;
+  } catch (error) {
+    throw new Error(`Error : ${error}`);
+  }
 };
