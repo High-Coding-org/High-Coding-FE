5b11f1dc3a1a4845bc4c84addc7c650a4dbf9d04
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-14 13:21:44 +0900

    [♻️refactor]: dto 수정에 따라 signIn post 경로 수정

--- src/services/auth/signIn.ts
+++ src/services/auth/signIn.ts
@@ -1,6 +1,7 @@
 import { SignInFormData } from '@/types/auth';
+import { SignInResponse } from '@/types/Login/signIn';
 
-import { API_ENDPOINT } from '../apiEndpoint';
+import { API_AUTHORITY, API_ENDPOINT } from '../apiEndpoint';
 import { axiosInstance } from '../axiosInstance';
 
 // ! 스웨거 속성 값 변경 시 username -> id로 변경해야 할 수 있음.
@@ -10,7 +11,10 @@ export const signIn = async ({ username, password }: SignInFormData) => {
     password,
   };
 
-  const res = await axiosInstance.post(API_ENDPOINT.AUTH.SIGNIN, reqBody);
+  const res: SignInResponse = await axiosInstance.post(
+    `${API_AUTHORITY.PUBLIC}${API_ENDPOINT.AUTH.SIGNIN}`,
+    reqBody
+  );
 
-  return res;
+  return res.data;
 };
