fca8fead8a6f7856001554cfbbf22da948d4cc33
김가연 <121287529+gayeon4679@users.noreply.github.com>
2025-03-15 12:59:45 +0900

    [♻️refactor]: 하드코딩된 profileDataKey 상수로 변경

--- src/hooks/api/useProfile.ts
+++ src/hooks/api/useProfile.ts
@@ -3,12 +3,13 @@ import { getProfile } from '@/services/profile/getProfile';
 import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
 
 import { IProfileData } from '../../pages/ProfilePage/type';
+import { PROFILE_DATA_KEY } from '@/constants/profileDataKey';
 
 export const useProfile = () => {
   const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN);
 
   return useQuery<IProfileData>({
-    queryKey: ['profileData', token],
+    queryKey: [PROFILE_DATA_KEY, token],
     queryFn: () => getProfile(token),
     retry: false,
   });
