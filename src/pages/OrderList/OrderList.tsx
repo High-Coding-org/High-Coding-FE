0d646e204fb1a9fc44c09e329f77ee34f036654b
타래 <85001878+meowTarae@users.noreply.github.com>
2025-03-20 13:54:38 +0900

    [🐛fix]: 머지되며 소실된 코드 복구

--- src/pages/OrderList/OrderList.tsx
+++ src/pages/OrderList/OrderList.tsx
@@ -1,16 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 
 import BreadcrumbAndTitle from '@/components/common/Breadcrumb/BreadcrumbAndTitle';
 import SideBar from '@/components/common/SideBar';
 import Spinner from '@/components/common/Spinner/Spinner';
-import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
-import { API_AUTHORITY, API_ENDPOINT } from '@/services/apiEndpoint';
-import { axiosInstance } from '@/services/axiosInstance';
+import { getOrderList } from '@/services/orderList/getOrderList';
 
 import OrderData from './OrderData';
-import { OrderListResponse } from './type';
 
 // ! 공통 컴포넌트 - SideBar 변경 후 더미데이터 제거 예정
 const DUMMY_SIDEBAR_DATA = [
@@ -24,23 +20,6 @@ const DUMMY_SIDEBAR_DATA = [
 ];
 
 export default function OrderList() {
-  const navigate = useNavigate();
-
-  const getOrderList = async () => {
-    const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN);
-
-    const res: OrderListResponse = await axiosInstance.get(
-      `${API_AUTHORITY.USER}${API_ENDPOINT.ORDER.ORDER_LIST}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    return res?.data;
-  };
-
   const { data: orderList, isLoading } = useQuery({
     queryKey: ['orderList'],
     queryFn: getOrderList,
@@ -77,5 +56,6 @@ export default function OrderList() {
           <SideBar menuItems={DUMMY_SIDEBAR_DATA} />
         </aside>
       </div>
-   }
+    </>
+  );
 }
