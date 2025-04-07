a8880b98ef048755cfff285313e1d55fc5968ade
타래 <85001878+meowTarae@users.noreply.github.com>
2025-04-07 22:32:49 +0900

    [♻️refactor]: 헤더 컴포넌트 경로를 상수화

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -1,5 +1,7 @@
 import { CircleHelp, CircleUserRound } from 'lucide-react';
 
+import { PATH } from '@/routes/path';
+
 export const MENU_ITEMS = [
   '키트 정보',
   '나의 식물',
@@ -8,14 +10,14 @@ export const MENU_ITEMS = [
 ];
 
 export const NAME_TO_PATH = {
-  '키트 정보': '/product',
-  '나의 식물': '/plant',
-  '식물 지식백과': '/plant/dictionary',
-  'AI 식물 추천': '/plant/AIRecommend',
-  '내 정보': '/profile',
-  '고객 센터': '/customer-center',
-  '로그인/로그아웃': '/sign',
-};
+  '키트 정보': PATH.PRODUCT,
+  '나의 식물': PATH.PLANT,
+  '식물 지식백과': `${PATH.PLANT}/${PATH.PLANT_DICTIONARY}`,
+  'AI 식물 추천': `${PATH.PLANT}/${PATH.PLANT_RECOMMEND}`,
+  '내 정보': PATH.PROFILE,
+  '고객 센터': PATH.CUSTOMER_CENTER,
+  '로그인/로그아웃': PATH.SIGN,
+} as const;
 
 export const ADDITIONAL_MENU_ITEMS = [
   '고객 센터',
