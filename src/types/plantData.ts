476488b36f2df5d8fc388afb7d4c6892ad9bb19d
타래 <85001878+meowTarae@users.noreply.github.com>
2025-04-28 21:22:44 +0900

    [✨feat]: 중복되는 타입 파일을 전역에 타입 파일에 생성

---
+++ src/types/plantData.ts
@@ -0,0 +1,9 @@
+export interface IPlantData {
+  plantName: string;
+  temperature: number | null;
+  humidity: number | null;
+  light: number | null;
+  soilMoisture: number | null;
+  goalGrowth: number | null;
+  image: string | null;
+}
