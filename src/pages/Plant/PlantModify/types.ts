b5de4d8ebc364272209deafb5c8c19c010512d8e
타래 <85001878+meowTarae@users.noreply.github.com>
2025-04-28 21:25:43 +0900

    [🔥remove]: 식물 등록 페이지 수정에 따른 기존 수정 페이지 제거

--- src/pages/Plant/PlantModify/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IPlantRegister {
-  plantName: string;
-  temperature: number | null;
-  humidity: number | null;
-  light: number | null;
-  soilMoisture: number | null;
-  goalGrowth: number | null;
-  image: string | null;
-}
