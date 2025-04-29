517275669b364c88678131ed7d4c1c82322dff34
타래 <85001878+meowTarae@users.noreply.github.com>
2025-04-29 16:51:47 +0900

    [✨feat]: 역할이 plant 인지 검사하는 route 기능 구현

--- src/routes/PlantPrivateRoute.tsx
+++ src/routes/PlantPrivateRoute.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 
 import { API_AUTHORITY, API_ENDPOINT } from '@/services/apiEndpoint';
 import { axiosInstance } from '@/services/axiosInstance';
+import { UserInfoResponse } from '@/types/userInfo';
 import { getUserToken } from '@/utils/getUserToken';
 
 import { PATH } from './path';
@@ -15,17 +16,22 @@ export default function PlantPrivateRoute({ page }: PlantPrivateRouteProps) {
   const navigate = useNavigate();
   const [hasAccess, setHasAccess] = useState(false);
 
-  const checkUserRole = async token => {
-    const res = await axiosInstance.get(
-      `${API_AUTHORITY.USER}${API_ENDPOINT.PROFILE}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+  const getUserRole = async (token: string) => {
+    try {
+      const res: UserInfoResponse = await axiosInstance.get(
+        `${API_AUTHORITY.USER}${API_ENDPOINT.PROFILE}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-    console.log(res);
+      return res?.data?.userInfo?.role;
+    } catch (e) {
+      alert(`API 요청에 실패했습니다. ${e}`);
+      navigate(PATH.HOME);
+    }
   };
 
   useEffect(() => {
@@ -37,7 +43,14 @@ export default function PlantPrivateRoute({ page }: PlantPrivateRouteProps) {
       return;
     }
 
-    checkUserRole(token);
+    getUserRole(token).then(role => {
+      if (role === 'ROLE_PLANT' || role === 'ROLE_ADMIN') {
+        setHasAccess(true);
+      } else {
+        alert('접근할 수 없는 페이지 입니다.');
+        navigate(PATH.HOME);
+      }
+    });
   }, [navigate]);
 
   return hasAccess ? page : null;
