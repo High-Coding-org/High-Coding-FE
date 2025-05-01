f076f3adc6b1e749bddfce1bd79453879381e9b8
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-01 12:27:44 +0900

    [🐛fix]: token valid 검사 api 오류나던 부분을 해결
    응답받는 status code 값에 따라 true를 반환하거나, error를 반환하는 형태로 변경

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,40 +1,45 @@
 import { useMutation } from '@tanstack/react-query';
 import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import Header from '@/components/common/Header/Header';
 import { LOCAL_STORAGE_AUTH_TOKEN } from '@/constants/localStorageKey';
+import { PATH } from '@/routes/path';
 import { checkTokenValid } from '@/services/auth/tokenValid';
+import { getUserToken } from '@/utils/getUserToken';
 
 interface LayoutProps {
   showHeader: boolean;
 }
 
 export function Layout({ showHeader }: LayoutProps) {
-  const { mutate } = useMutation({
-    mutationFn: checkTokenValid,
+  const useCheckTokenValid = () => {
+    const navigate = useNavigate();
 
-    onError: () => {
-      toast.error(
-        <div className="flex flex-col gap-2">
-          <p>로그인 세션이 만료되었습니다.</p>
-          <p>다시 로그인해주세요.</p>
-        </div>
-      );
-      localStorage.removeItem(LOCAL_STORAGE_AUTH_TOKEN);
-    },
-  });
-  // [✨feat]: axiosInstance timout 시간 증가
+    return useMutation({
+      mutationFn: checkTokenValid,
+      onError: () => {
+        toast.error(
+          <div className="flex flex-col gap-2">
+            <p>로그인 세션이 만료되었습니다.</p>
+            <p>다시 로그인해주세요.</p>
+          </div>
+        );
+        localStorage.removeItem(LOCAL_STORAGE_AUTH_TOKEN);
+        navigate(PATH.HOME);
+      },
+    });
+  };
+
+  const { mutate } = useCheckTokenValid();
+
   useEffect(() => {
+    const token = getUserToken();
+    if (!token) return;
+
     mutate();
   }, [mutate]);
-
-  // useEffect(() => {
-  //   if (isSuccess && !isTokenValid) {
-  //     localStorage.removeItem(LOCAL_STORAGE_AUTH_TOKEN);
-  //   }
-  // }, [isTokenValid, isSuccess]);
 
   return (
     <div
