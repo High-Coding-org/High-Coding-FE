f8b268c97febd9e7a2e64e16f3f397e4346ea613
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-04 16:20:32 +0900

    [✨feat]: api 데이터 연동

--- src/pages/Plant/PlantDiary/PlantDiary.tsx
+++ src/pages/Plant/PlantDiary/PlantDiary.tsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
 
 import BreadcrumbAndTitle from '@/components/common/Breadcrumb/BreadcrumbAndTitle';
-import { usePlantDiary } from '@/hooks/api/usePlant';
+import { usePlantById, usePlantDiary } from '@/hooks/api/usePlant';
+import { formatPlantDiaryDate } from '@/utils/formatDate';
 
 import PlantCalendar from './components/PlantCalendar';
 import PlantDiaryForm from './PlantDiaryForm/PlantDiaryForm';
 
 export default function PlantDiary() {
-  const [selectDate, setSelectDate] = useState<Date>();
+  const [selectDate, setSelectDate] = useState<Date>(new Date());
   const { id } = useParams();
   const navigate = useNavigate();
-
-  const { data: plantData, isLoading, isError } = usePlantDiary(Number(id));
+  const { data: plantData, isLoading, isError } = usePlantById(Number(id));
+  const { data: plantDiaryData } = usePlantDiary(
+    Number(id),
+    formatPlantDiaryDate(selectDate)
+  );
 
   useEffect(() => {
     if (!id) {
@@ -20,37 +24,8 @@ export default function PlantDiary() {
     }
   }, [id, navigate]);
 
-  /**
-   * get 요청 시 리턴값
-    {
-      "plantId": 1,
-      "date": "2025-05-01",
-      "growth": null,
-      "content": null,
-      "totalGrowth": 0,
-      "percentage": 0
-    }
-  */
-
-  /*
-    ///? URL에 입력된 ID가 유효한 값을 가지고 있는지 검사
-   
-    const {
-      data: plants,
-      isLoading,
-      isError,
-    } = useQuery({
-      queryKey: [PLANT_DIARY_QUERY_KEY, id],
-      queryFn: () => getPlantById(id),
-    });
-
-    useEffect(() => {
-      if (!isError) return;
-
-      navigate(-1);
-      toast.error('해당 식물 조회에 실패했습니다.');
-    }, [isError, navigate]);
-  */
+  //? URL에 입력된 ID가 유효한 값을 가지고 있는지 검사
+  //? isError를 통해
 
   return (
     <>
@@ -58,7 +33,10 @@ export default function PlantDiary() {
 
       <main className="flex gap-8 w-pageWidth">
         <section className="flex-1 p-6 space-y-6 border rounded-lg">
-          <PlantDiaryForm {...plantData} />
+          <PlantDiaryForm
+            {...plantData}
+            {...plantDiaryData}
+          />
         </section>
 
         <section className="flex items-center justify-center flex-1 border rounded-lg">
--- src/pages/Plant/PlantDiary/PlantDiaryForm/PlantDiaryForm.tsx
+++ src/pages/Plant/PlantDiary/PlantDiaryForm/PlantDiaryForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
@@ -7,8 +9,10 @@ import PlantInfoSection from './PlantInfo';
 export default function PlantDiaryForm({
   name,
   percentage,
-  totalGrowth,
   imageUrl,
+  content: initialContent,
+  growth: initialGrowth,
+  totalGrowth,
 }) {
   const plantInfo = {
     name,
@@ -17,23 +21,29 @@ export default function PlantDiaryForm({
     imageUrl,
   };
 
+  const [content, setContent] = useState(initialContent);
+  const [growth, setGrowth] = useState(initialGrowth);
+
   return (
     <div className="space-y-6">
       <PlantInfoSection {...plantInfo} />
 
       <div className="space-y-2">
-        <label className="text-sm font-medium">길이</label>
-        <Input
-          type="text"
-          placeholder="16cm"
-        />
+        <label className="text-sm font-medium">길이 (cm)</label>
+        <div className="flex items-center gap-2">
+          <Input
+            type="number"
+            placeholder="현재 길이를 입력해주세요."
+          />
+          <Button>입력하기</Button>
+        </div>
       </div>
 
       <div className="space-y-2">
         <label className="text-sm font-medium">오늘의 일기</label>
         <Textarea
           className="h-[240px] resize-none"
-          placeholder="오늘 하루 식물의 변화를 기록해보세요"
+          placeholder="오늘 하루 식물의 변화를 기록해보세요."
         />
       </div>
 
