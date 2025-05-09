5080aca23fc5f0e0f393dcedc6b0f277cf62228b
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-09 17:45:09 +0900

    [✨feat]: 키트 상세 페이지에 이미지 추가

---
+++ src/pages/Kit/KitDetailPage/constants/images.ts
@@ -0,0 +1,4 @@
+export const KitDetailPageImages = {
+  thumbnail: new URL('@/assets/kit/smartFarmKit.webp', import.meta.url).href,
+  kitDetail: new URL('@/assets/kit/kitDetail.webp', import.meta.url).href,
+};
