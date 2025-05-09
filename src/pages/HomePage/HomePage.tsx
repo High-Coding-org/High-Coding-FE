b18e16df612bbc4a869280f42fdb7ada6a157ed2
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-09 14:00:03 +0900

    [✨feat]: 홈페이지 파트2 구현

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -10,14 +10,16 @@ import { useGlobalErrorStore } from '@/store/globalErrorStore';
 
 import ResponsiveText from './components/ResponsiveText';
 import {
-  function1Class,
-  function1SubClass,
-  function1SubText,
-  function1Text,
-  function2Class,
-  function2SubText,
-  function2SubTextClass,
-  function2Text,
+  characteristics_Class,
+  characteristics_SubText,
+  characteristics_SubTextClass,
+  characteristics_Text,
+  part1_Class,
+  part1_SubClass,
+  part1_SubText,
+  part1_Text,
+  part2_SubText,
+  part2_Text,
   subTitle,
   subTitleClass,
   title,
@@ -33,6 +35,10 @@ export default function HomePage() {
   ).href;
   const homeImage1 = new URL(
     '@/assets/HomePage/HomeImage-1.webp',
+    import.meta.url
+  ).href;
+  const homeImage2 = new URL(
+    '@/assets/HomePage/HomeImage-2.webp',
     import.meta.url
   ).href;
 
@@ -99,12 +105,90 @@ export default function HomePage() {
         <article
           data-aos="fade-up"
           className="mt-[160px]">
-          {function1Text.map((text, index) => (
+          {part1_Text.map((text, index) => (
             <ResponsiveText
-              key={`${index}-function1Text`}
+              key={`${index}-part1Text`}
               text={text}
               breakPoint="|"
-              className={function1Class}
+              className={part1_Class}
+            />
+          ))}
+        </article>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="bottom-bottom"
+          className="my-8 md:absolute md:top-1/4 md:right-9 w-[480px] h-[540px] border-2 rounded-md">
+          <img
+            src={homeImage1}
+            alt="HomeImage1"
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <span
+          data-aos="fade-up"
+          className="text-xl md:absolute md:bottom-[160px] md:left-9">
+          {part1_SubText.map((text, index) => (
+            <ResponsiveText
+              key={`${index}-part1SubText`}
+              text={text}
+              breakPoint="|"
+              className={part1_SubClass}
+            />
+          ))}
+        </span>
+      </section>
+
+      {/* 파트 2: 식물을 체계적으로 관리해 보세요 */}
+      <section className="h-[1080px] bg-[#F9FAFB] w-full flex justify-center">
+        <div className="flex  md:h-[960px] md:w-[1140px] md:relative md:px-8">
+          <article
+            data-aos="fade-up"
+            className="mt-[160px]">
+            {part2_Text.map((text, index) => (
+              <ResponsiveText
+                key={`${index}-part2Text`}
+                text={text}
+                breakPoint="|"
+                className={part1_Class}
+              />
+            ))}
+          </article>
+          <div
+            data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom"
+            className="md:absolute md:top-1/4 md:right-5 w-[600px] h-[680px] border-2 rounded-md">
+            <img
+              src={homeImage2}
+              alt="HomeImage2"
+              className="object-contain w-full h-full scale-110 bg-gray-50"
+            />
+          </div>
+          <span
+            data-aos="fade-up"
+            className="text-xl md:absolute md:bottom-[160px] md:left-9">
+            {part2_SubText.map((text, index) => (
+              <ResponsiveText
+                key={`${index}-part2SubText`}
+                text={text}
+                breakPoint="|"
+                className={part1_SubClass}
+              />
+            ))}
+          </span>
+        </div>
+      </section>
+
+      {/* 파트 3: 처음 키우는 식물도 어려움 없이 */}
+      <section className="h-[1080px] md:h-[960px] md:w-[1140px] md:relative md:px-8">
+        <article
+          data-aos="fade-up"
+          className="mt-[160px]">
+          {part1_Text.map((text, index) => (
+            <ResponsiveText
+              key={`${index}-part1Text`}
+              text={text}
+              breakPoint="|"
+              className={part1_Class}
             />
           ))}
         </article>
@@ -121,12 +205,12 @@ export default function HomePage() {
         <span
           data-aos="fade-up"
           className="text-xl md:absolute md:bottom-[160px] md:left-9">
-          {function1SubText.map((text, index) => (
+          {part1_SubText.map((text, index) => (
             <ResponsiveText
-              key={`${index}-function1SubText`}
+              key={`${index}-part1SubText`}
               text={text}
               breakPoint="|"
-              className={function1SubClass}
+              className={part1_SubClass}
             />
           ))}
         </span>
@@ -138,30 +222,30 @@ export default function HomePage() {
           <article
             data-aos="fade-up"
             className="mt-[160px]">
-            {function2Text.map((text, index) => (
+            {characteristics_Text.map((text, index) => (
               <ResponsiveText
-                key={`${index}-function2Text`}
+                key={`${index}-characteristicsText`}
                 text={text}
                 breakPoint="|"
-                className={function2Class}
+                className={characteristics_Class}
               />
             ))}
           </article>
 
           <div className="grid grid-cols-1 gap-16 mt-20 md:grid-cols-2 ">
-            {function2SubText.map((obj, index) => (
+            {characteristics_SubText.map((obj, index) => (
               <div
-                key={`Fnc2 describe-${index}`}
+                key={`Characteristics describe-${index}`}
                 data-aos="fade-up">
                 <Droplet className="w-20 h-20 text-blue-500" />
-                <span className="my-4 text-2xl font-semibold text-gray-700">
+                <span className="block my-4 text-2xl font-semibold text-gray-700">
                   {obj.title}
                 </span>
-                <span className="text-base font-semibold text-gray-500">
+                <span className="text-base font-semibold text-gray-500 ">
                   <ResponsiveText
                     text={obj.subTitle}
                     breakPoint="|"
-                    className={function2SubTextClass}
+                    className={characteristics_SubTextClass}
                   />
                 </span>
               </div>
--- src/pages/HomePage/constants.ts
+++ src/pages/HomePage/constants.ts
@@ -5,14 +5,20 @@ export const subTitle = [
   '복잡한 설치 없이,  누구나 쉽고 직관적으로 식물을 관리할 수 있습니다.',
   '스마트한 자동화 식물 관리,  직접 체험해 보세요!',
 ];
-export const function1Text = ['처음 키우는 식물도', '어려움 없이'];
-export const function1SubText = [
+export const part1_Text = ['처음 키우는 식물도', '어려움 없이'];
+export const part1_SubText = [
   '스마트팜으로 시작하세요.',
   '이제는 누구나 손쉽게',
   '식물 전문가처럼 키울 수 있습니다.',
 ];
-export const function2Text = ['하이코딩만의', '특별한 기능들'];
-export const function2SubText = [
+export const part2_Text = ['식물을 체계적으로', '관리해 보세요'];
+export const part2_SubText = [
+  '식물 성장 일기 페이지에서',
+  '식물의 상태를 간편하게 기록하고',
+  '생장 패턴을 시각적으로 확인할 수 있어요.',
+];
+export const characteristics_Text = ['하이코딩만의', '특별한 기능들'];
+export const characteristics_SubText = [
   {
     title: '자동 급수 시스템',
     subTitle:
@@ -34,16 +40,17 @@ export const function2SubText = [
       '물 공급 자동화를 통해 식물의 건강을 유지하고 | 최적의 성장 환경을 제공합니다.',
   },
 ];
-export const function3Text = ['어떤 식물을 키울지', '쉽게 알 수 있어요'];
+export const part3_Text = ['어떤 식물을 키울지', '쉽게 알 수 있어요'];
 
 // Class
 export const titleClass =
   ' text-6xl font-bold leading-normal text-center whitespace-pre sm:text-4xl sm:font-semibold sm:leading-normal';
 export const subTitleClass =
   'text-3xl font-semibold leading-relaxed tracking-tight text-center whitespace-pre sm:text-2xl sm:font-normal';
-export const function1Class =
+export const part1_Class =
   'text-4xl font-bold leading-relaxed tracking-tight sm:text-2xl';
-export const function1SubClass = 'text-2xl';
-export const function2Class =
+export const part1_SubClass = 'text-2xl leading-relaxed tracking-tight';
+export const characteristics_Class =
   'text-4xl font-bold leading-relaxed tracking-tight sm:text-2xl';
-export const function2SubTextClass = 'text-base font-semibold text-gray-500';
+export const characteristics_SubTextClass =
+  'text-base font-semibold text-gray-500';
